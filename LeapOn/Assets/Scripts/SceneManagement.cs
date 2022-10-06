0c88a043301dbea412c2b82cb750d84ff68fcafd
Berkay Yelekçi <mberkay.yelekci@gmail.com>
2022-10-06 16:39:04 +0300

    Menu added
    Menu with animations has created.
    Playbutton has animation.

---
+++ LeapOn/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+public class SceneManagement : MonoBehaviour
+{
+    public RectTransform mainMenu;
+
+
+    private void Start()
+    {
+        MenuAnimation();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void  MenuAnimation()
+    {
+        mainMenu.DOLocalMove(Vector2.zero, 1f);
+    }
+
+}
