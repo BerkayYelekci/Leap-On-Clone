07430596629baa7dac8b260127aa46bedc4fbcfd
YusaCan <ahmedyusacan@gmail.com>
2022-10-06 12:03:19 +0300

    Scriptable System Created
    Bool, int and float scriptable objects created

---
+++ LeapOn/Assets/Scripts/ScriptableBool.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableBool", menuName = "ScriptableBool")]
+public class ScriptableBool : ScriptableObject
+{
+    public bool value;
+}
---
+++ LeapOn/Assets/Scripts/ScriptableInt.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableInt" , menuName = "ScriptableInt")]
+public class ScriptableInt : ScriptableObject
+{
+    public int value;
+}
