85ea4f97e3115b8ff6777829161dbd3ba9eaa10a
YusaCan <ahmedyusacan@gmail.com>
2022-10-10 17:09:29 +0300

    LineBetweenBallAndCenter Updated
    Now the line is visible to player on game scene

--- LeapOn/Assets/Scripts/LineBetweenBallAndCenter.cs
+++ LeapOn/Assets/Scripts/LineBetweenBallAndCenter.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class LineBetweenBallAndCenter : MonoBehaviour
 {
     public Transform center;
+    LineRenderer lineRenderer;
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, center.position);   
+    }
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, center.position);
+        lineRenderer.SetPosition(1, transform.position);
     }
 }
