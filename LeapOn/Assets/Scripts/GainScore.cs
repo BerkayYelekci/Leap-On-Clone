7683101912f13cf79b086f29ac4acb0cf2c1a2bf
YusaCan <ahmedyusacan@gmail.com>
2022-10-10 18:27:20 +0300

    GameScene Updated

--- LeapOn/Assets/Scripts/GainScore.cs
+++ LeapOn/Assets/Scripts/GainScore.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class GainScore : MonoBehaviour
 {
     public ScriptableInt score;
+    public ScriptableInt scoreMultiplier;
     int incrementScore;
     private void Start()
     {
         score.value = 0;
         // It will increment when player passes the multiplier line
-        incrementScore = 1;
+        scoreMultiplier.value = 0;
     }
     private void OnEnable()
     {
         PlayerMovement.gainScore += Gain;
+        MultiplyScore.multiplyTheScore += Multiplier;
     }
     private void OnDisable()
     {
        PlayerMovement.gainScore -= Gain;
+       MultiplyScore.multiplyTheScore -= Multiplier;
     }
     void Gain()
     {
-        score.value += incrementScore;
+        score.value += scoreMultiplier.value;
+    }
+    void Multiplier()
+    {
+        scoreMultiplier.value += 1;
+        // Multiply number appears in UI
+
+        ////////////////////////////////
     }
 }
