e43ce4dd6348d2a252fbc7424be473f19276ae26
YusaCan <ahmedyusacan@gmail.com>
2022-10-10 17:09:58 +0300

    GameManager Updated
    Game over mechanic enabled

--- LeapOn/Assets/Scripts/GameManager.cs
+++ LeapOn/Assets/Scripts/GameManager.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public ScriptableBool isJumping;
+    public ScriptableBool gameOver;
+    private void OnEnable()
+    {
+        DestroyPlayer.gameOver += GameOver;
+    }
+    private void OnDisable()
+    {
+        DestroyPlayer.gameOver -= GameOver;
+    }
     void Awake()
     {
         isJumping.value = false;
+        gameOver.value = false;
+    }
+    void GameOver()
+    {
+        gameOver.value = true;
     }
 }
