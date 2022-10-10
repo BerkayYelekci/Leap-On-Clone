b74b6433c0d2b126411ccd73241176a0da07055c
YusaCan <ahmedyusacan@gmail.com>
2022-10-10 12:40:28 +0300

    PlayerMovement Script Updated
    Player movement mechanics now are more efficient

--- LeapOn/Assets/Scripts/PlayerMovement.cs
+++ LeapOn/Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,10 @@ public class PlayerMovement : MonoBehaviour
     public ScriptableBool isJumping;
 
     public float rotateSpeed; 
-
     public float fallSpeed;
+    public float jumpSpeed;
+
+    float jumpTime;
 
     public GameObject chainSaw;
     Vector2 center;
@@ -17,23 +19,41 @@ public class PlayerMovement : MonoBehaviour
     float radius;
 
     void Start()
-    {    
+    {
+        jumpTime = .4f;
         center = chainSaw.transform.position;
     }
     void Update()
     {
+        // The ball always looks at the center
+        LookCenter();
         // If not jumping
-        //FallOnGround();
-        /////////////////
+        if (!isJumping.value)
+        {
+            FallOnGround();
+        }
+        else
+        {
+            Jump();
+        }
+        // If player touches to the screen, the ball will rotate wheter its jumping or not
         if (Input.GetMouseButton(0))
         {
             RotatePlayer();
         }
+        /////////////////   
+    }
+    void LookCenter()
+    {
+        Vector2 direction = (center - (Vector2)transform.position).normalized;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        var offset = 90f;
+        transform.rotation = Quaternion.Euler(Vector3.forward * (angle + offset));
     }
     // Rotates player clock-wise
     void RotatePlayer()
     {
-        // Hem rotate et (LookAt kullan) hem de move et     
+        // Hem rotate et (LookAt kullan) hem de move et    
         radius = Vector2.Distance(transform.position, chainSaw.transform.position);
         angle += rotateSpeed * Time.deltaTime; // Güncellenecek
         offset = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * radius;
@@ -43,5 +63,24 @@ public class PlayerMovement : MonoBehaviour
     {
         float speed = fallSpeed * Time.deltaTime; 
         transform.position = Vector2.MoveTowards(transform.position, center, speed);
+    }
+    void Jump()
+    {
+        // 1 second jump time always being less and less until it reaches to 0
+        jumpTime -= Time.deltaTime;
+        transform.Translate(Vector2.up * jumpSpeed * Time.deltaTime);
+        if (jumpTime <= 0)
+        {
+            isJumping.value = false;
+        }
+        // Jump mechanic
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("White"))
+        {
+            isJumping.value = true;
+            jumpTime = .4f;
+        }
     }
 }
