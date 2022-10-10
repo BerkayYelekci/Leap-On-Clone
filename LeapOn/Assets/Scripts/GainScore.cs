0ca8b82f5980f7e845a0dd4ac313256bceb26685
YusaCan <ahmedyusacan@gmail.com>
2022-10-10 13:00:05 +0300

    GainScore Script Created
    Player gains score when collides with the white objects

---
+++ LeapOn/Assets/Scripts/GainScore.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GainScore : MonoBehaviour
+{
+    public ScriptableInt score;
+    int incrementScore;
+    private void Start()
+    {
+        score.value = 0;
+        // It will rise incremental when player exceeds the multiplier line
+        incrementScore = 1;
+    }
+    private void OnEnable()
+    {
+        PlayerMovement.gainScore += Gain;
+    }
+    private void OnDisable()
+    {
+        PlayerMovement.gainScore -= Gain;
+    }
+    void Gain()
+    {
+        score.value += incrementScore;
+    }
+}
