571aac84d4da31b0ef93206f7a665a7ff31c49e9
Berkay Yelekçi <mberkay.yelekci@gmail.com>
2022-10-13 14:32:21 +0300

    Particle on obstacles added.

--- LeapOn/Assets/Scripts/DestroyObstacles.cs
+++ LeapOn/Assets/Scripts/DestroyObstacles.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class DestroyObstacles : MonoBehaviour
 {
+    [SerializeField] GameObject impactParticles;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             transform.parent.gameObject.SetActive(false);
+            ImpactEffect();
         }
+    }
+    private void ImpactEffect()
+    {
+        GameObject explosion = Instantiate(impactParticles, transform.position, Quaternion.identity);
+        explosion.GetComponent<ParticleSystem>().Play();
+        Destroy(explosion, .1f);
     }
 }
