daaf053277419143644c6ecb1806413e45c153ae
YusaCan <ahmedyusacan@gmail.com>
2022-10-14 16:51:21 +0300

    Collider Problem Solved
    A bug that colliders caused has solved

--- LeapOn/Assets/Scripts/DestroyObstacles.cs
+++ LeapOn/Assets/Scripts/DestroyObstacles.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class DestroyObstacles : MonoBehaviour
 {
     [SerializeField] GameObject impactParticles;
-
+    public Collider2D otherCollider;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            otherCollider.enabled = false;
             transform.parent.gameObject.SetActive(false);
             ImpactEffect();
         }
--- LeapOn/Assets/Scripts/DestroyPlayer.cs
+++ LeapOn/Assets/Scripts/DestroyPlayer.cs
@@ -8,11 +8,13 @@ public class DestroyPlayer : MonoBehaviour
     public AudioSource failAS;
     public AudioClip failAC;
     public static Action gameOver;
+    public Collider2D otherCollider;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            otherCollider.enabled = false;
             // Setactive gameovercanvas
             // Destroys player 
             // GAME OVER
