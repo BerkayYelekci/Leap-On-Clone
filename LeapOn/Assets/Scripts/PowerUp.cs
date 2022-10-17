7771bba27b4a0111ca42d47c78b1dbbfbb30d73e
YusaCan <ahmedyusacan@gmail.com>
2022-10-17 12:58:13 +0300

    PowerUp Script Updated
    RotateSpeed Power up integration

--- LeapOn/Assets/Scripts/PowerUp.cs
+++ LeapOn/Assets/Scripts/PowerUp.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PowerUp : MonoBehaviour
 {
     int randomPowerUp;
-    bool jumpPU, fastPU;
+    float powerUpTime;
+    bool jumpPU, rotatePU;
     public GameObject jumpIcon, fastIcon;
+    public ScriptableFloat rotateSpeed;
     private void Awake()
     {
+        powerUpTime = 1.5f;
         randomPowerUp = Random.Range(0, 2);
         // Jump Higher
         if (randomPowerUp == 0)
         {
             fastIcon.gameObject.SetActive(false);
-            fastPU = false;
+            rotatePU = false;
             jumpPU = true;
         }
         // RotateFaster
@@ -22,17 +25,30 @@ public class PowerUp : MonoBehaviour
         {
             jumpIcon.gameObject.SetActive(false);
             jumpPU = false;
-            fastPU = true;
+            rotatePU = true;
         }
     }
-    void Start()
+    IEnumerator IncreaseRotateSpeed()
     {
-        
+        rotateSpeed.value = 2;
+        yield return new WaitForSeconds(powerUpTime);
+        gameObject.SetActive(false);
+        rotateSpeed.value = 1;
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator IncreaseJumpPower()
     {
-        
+        yield return new WaitForSeconds(powerUpTime);
+        gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player") && rotatePU)
+        {
+            StartCoroutine(IncreaseRotateSpeed());
+        }
+        else if (collision.gameObject.CompareTag("Player") && jumpPU)
+        {
+            StartCoroutine(IncreaseJumpPower());
+        }
     }
 }
